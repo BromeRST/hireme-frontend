71394d1a78e1b6100fdaca5b5c3c7620fa22d2ca
Massimiliano <94859244+BromeRST@users.noreply.github.com>
2022-09-20 10:06:20 +0200

    added jobs board page

--- components/_shared/NavBar.jsx
+++ components/_shared/NavBar.jsx
@@ -1,10 +1,11 @@
 import { navElements } from "../../lib/constants";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import HireMe from "../../context/context";
 import Link from "next/link";
 
 const NavBar = () => {
   const { connect, wallet, provider, signer } = useContext(HireMe);
+  const [selected, setSelected] = useState();
 
   return (
     <div className="py-6 px-28 2xl:px-52 flex items-center justify-between border-b border-gray-300">
@@ -18,11 +19,38 @@ const NavBar = () => {
           </div>
         </Link>
         <div className="flex items-center gap-8 font-medium text-gray-500">
-          {navElements.map((el, i) => (
-            <div key={i} className="cursor-pointer hover:text-black">
-              {el}
-            </div>
-          ))}
+          {navElements.map((el, i) => {
+            if (i === 0) {
+              if (signer) {
+                return (
+                  <Link href="/jobs-board">
+                    <div
+                      key={i}
+                      onClick={() => setSelected(i)}
+                      className={`w-28 text-center cursor-pointer hover:text-black ${
+                        selected === i &&
+                        "text-blue-700 py-2 rounded-lg bg-blue-50"
+                      }`}
+                    >
+                      {el}
+                    </div>
+                  </Link>
+                );
+              }
+            } else {
+              return (
+                <div
+                  key={i}
+                  onClick={() => setSelected(i)}
+                  className={`w-28 text-center cursor-pointer hover:text-black ${
+                    selected === i && "text-blue-700 py-2 rounded-lg bg-blue-50"
+                  }`}
+                >
+                  {el}
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
       <div
